817e311989839dd140d2d55f79400fb8ceb3c40a
Ronei Macedo Gualberto <ronei.mac.rmg@gmail.com>
2016-01-18 12:54:19 -0400

    Arquivos iniciais do curso de nodejs

---
+++ produtos.js
@@ -0,0 +1,3 @@
+console.log('listagem de produtos');
+
+
---
+++ produtosWeb.js
@@ -0,0 +1,19 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+
+	console.log('url', req.url);
+	if(req.url == "/produtos"){
+		res.end("<html><body><h1> Listando os produtos</h1></body></html>");
+	}
+	else {
+		res.end("<html><body> Home da casa do código</body></html>");
+	}
+
+
+	
+}).listen(3000,"localhost");
+
+console.log('Servidor rodando');
+
+
